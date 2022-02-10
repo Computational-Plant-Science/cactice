4c10d95e3db573640d0e2570d57e4993699bb65d
w-bonelli <wpb3hw@virginia.edu>
2022-02-10 12:17:48 -0500

    sample notebook for reading grids from CSV and plotting them

--- cactice/vis.py
+++ cactice/vis.py
@@ -5,12 +5,12 @@ import numpy as np
 import seaborn as sns
 
 
-def show_plot(plot: np.ndarray, title: Optional[str] = None, cmap='Greens') -> None:
-    plt.figure(figsize=(20, 20))
-    values = list(set(np.ravel(plot)))
-    labels = np.vectorize(lambda x: str(int(x)) if x != 0 else '')(plot)
+def show_grid(grid: np.ndarray, title: Optional[str] = None, cmap='Greens') -> None:
+    # plt.figure(figsize=(20, 20))
+    values = list(set(np.ravel(grid)))
+    labels = np.vectorize(lambda x: str(int(x)) if x != 0 else '')(grid)
     ax = sns.heatmap(
-        plot,
+        grid,
         square=True,
         cbar=False,
         annot=labels,
