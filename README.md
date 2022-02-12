cd621524846a37c3d3bc7ecbe09d1aa2e2143f76
w-bonelli <wpb3hw@virginia.edu>
2022-02-12 14:30:54 -0500

    scaffolding for KNN and MRF models, update README

--- README.md
+++ README.md
@@ -3,3 +3,13 @@
 A Python3 toolkit for **C**omputing **A**gricultural **C**ategorical lat**TICE**s: category-valued grids such as crop species or phenotype classes arranged in a field or greenhouse.
 
 **This repository is experimental and under active development.**
+
+## Overview
+
+TODO
+
+## Conventions
+
+This library makes several assumptions about datasets to which the user must conform:
+
+- Class values are parsed as strings (and mapped internally to integers). Each distinct string is a class, regardless of numeric value: for instance, `9.5` and `9.5000` are considered distinct.
---
+++ cactice/mrf.py
@@ -0,0 +1,33 @@
+from typing import List
+
+import numpy as np
+
+from cactice.enums import Neighbors
+
+
+class MRF:
+    def __init__(self, neighbors: Neighbors = Neighbors.CARDINAL, layers: int = 1):
+        """
+        Initialize a Markov random field model.
+
+        :param neighbors: Which adjacent cells to consider neighbors.
+        :param layers: How many layers of adjacent cells to consider neighbors.
+        """
+        self.__neighbors = neighbors
+        self.__layers = layers
+
+    def fit(self, grids: List[np.ndarray]):
+        """
+        Fit the model to the given grids (precompute neighborhoods and probability distribution).
+        """
+        pass
+
+    def predict(self, grids: List[np.ndarray] = None):
+        """
+        Impute missing cells on the grids used to fit the model (or on the given grids, if provided).
+        To generate entirely new predictions conditioned on the training set, provide a list of empty (zero) grids.
+
+        :param grids: The grids to predict on (the training set will be used otherwise)
+        :return: The grids with missing values filled in.
+        """
+        pass
